8c41bc8fe89fa945e47d33354acde83e3c224f06
bilalkalac <76120196+bilalkalac@users.noreply.github.com>
2022-09-25 12:42:00 +0300

    Add files via upload

---
+++ src/notortalaması.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class notortalaması {
+    public static void main(String[] args) {
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Matematik Notunuzu Giriniz: ");
+        mat = inp.nextInt();
+
+        System.out.print("fizik Notunuzu Giriniz: ");
+        fizik = inp.nextInt();
+
+        System.out.print("kimya Notunuzu Giriniz: ");
+        kimya = inp.nextInt();
+
+        System.out.print("Turkce Notunuzu Giriniz: ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuzu Giriniz: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Muzik Notunuzu Giriniz: ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+        double sonuc = toplam/6.0 ;
+        System.out.print(sonuc);
+
+
+
+    }
+}
